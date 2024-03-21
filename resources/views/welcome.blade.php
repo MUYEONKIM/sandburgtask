661590d8120951f36d35c6c300fac3651053b6d5
MUYEONKIM <rlaandus2@gmail.com>
2024-03-21 19:55:21 +0900

    more fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.9">
+    <meta
+      http-equiv="Content-Security-Policy"
+      content="upgrade-insecure-requests" >
     <title>Document</title>
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
